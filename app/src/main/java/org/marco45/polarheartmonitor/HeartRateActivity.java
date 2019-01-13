657707e5b6500210f4b0e8c6352fcd64064cda3f
Frederick Bousson <fbousson@gmail.com>
2019-01-13 17:29:50 +0100

    playing

---
+++ app/src/main/java/org/marco45/polarheartmonitor/HeartRateActivity.java
@@ -0,0 +1,8 @@
+package org.marco45.polarheartmonitor;
+
+import android.app.Activity;
+
+public abstract class HeartRateActivity extends Activity {
+
+    public abstract void connectionError();
+}
